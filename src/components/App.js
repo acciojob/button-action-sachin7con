a70dabee192da7976f21101b7e7758632fbf80a3
sachin7con <sachin7con@gmail.com>
2024-08-17 17:33:40 +0530

    first Change

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  const[isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(prevState => !prevState);
+  };
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {/*  SGN */}
+      <p className={isVisible ? 'hide' : 'show'} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+
+      <button id="click" onClick={toggleVisibility} >{isVisible ? 'Show' : 'Hide' } </button>
     </div>
   );
 }
