dab3208045e01c7cd404975e66e0e830439125b5
sachin7con <sachin7con@gmail.com>
2024-08-17 17:42:10 +0530

    commit 3

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-  const[isVisible, setIsVisible] = useState(true);
+  // Set initial state to false to start with the paragraph hidden
+  const [isVisible, setIsVisible] = useState(false);
 
+  // Function to toggle visibility
   const toggleVisibility = () => {
     setIsVisible(prevState => !prevState);
   };
 
   return (
     <div className="App" id="main">
-      {/*  SGN */}
-      <p className={isVisible ? 'hide' : 'show'} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      {/* SGN */}
+      <p className={isVisible ? 'show' : 'hide'} id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+      </p>
 
-      <button id="click" onClick={toggleVisibility} >{isVisible ? 'Show' : 'Hide' } </button>
+      <button id="click" onClick={toggleVisibility}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
     </div>
   );
 }
 
-export default App
+export default App;
